d4634c8de766da6da6b0920e6b8a2b83ad83be49
Justin Blackwood <jblackwood12@my.whitworth.edu>
2012-05-02 20:11:11 -0700

    modified graphing

--- rails_app/public/javascripts/advanced_building.js
+++ rails_app/public/javascripts/advanced_building.js
@@ -35,56 +35,76 @@ function draw_chart() {
 			jQuery.post('/ajax_update', {building: abr, type: update_types[tab_index - 1], from: from_date, to: to_date}, function(response_data) {
 				var rd = eval(response_data).result;
 				var data = new google.visualization.DataTable();
-				data.addColumn('string', 'Hour');
-				data.addColumn('number', 'KWh');
-				data.addRows(rd.length + 1);
+				data.addColumn('date', 'Date');
+				data.addColumn('number', 'Power Usage (KWh)');
+				       /* data.addRows([
+				          [new Date(2008, 1 ,1), 30000, undefined, undefined, 40645, undefined, undefined],
+				          [new Date(2008, 1 ,2), 14045, undefined, undefined, 20374, undefined, undefined],
+				          [new Date(2008, 1 ,3), 55022, undefined, undefined, 50766, undefined, undefined],
+				          [new Date(2008, 1 ,4), 75284, undefined, undefined, 14334, 'Out of Stock','Ran out of stock on pens at 4pm'],
+				          [new Date(2008, 1 ,5), 41476, 'Bought Pens','Bought 200k pens', 66467, undefined, undefined],
+				          [new Date(2008, 1 ,6), 33322, undefined, undefined, 39463, undefined, undefined]
+				        ]);*/
+
+				//Label the graph using the data points stored in rd (an array of power usage readings)
+				label_graph(data, graph_type, rd, from_date, to_date);
+
+				//Create the chart, place it in the advanced_chart div located on the building view
+				var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('advanced_chart'));
 				
-				//Label the graph using the appropriate labels, based on the type of the graph
-				if(graph_type == "Hours"){
-					data.setValue(0, 0, "12 AM");
-					data.setValue(6, 0, "6 AM");
-					data.setValue(12, 0, "12 PM");
-					data.setValue(18, 0, "6 PM");
-				}else if(graph_type == "Month"){
-
-				}else if(graph_type == "FullYear"){
-
-				}else if(graph_type == "YTD"){
-
-				}else{
-					//Don't know how you got here
-				}
-				
-				
-							
-				for (var i = 0; i < rd.length + 1; ++i) {
-					data.setValue(i, 1, rd[i]);
-				}
-
-				var chart = new google.visualization.LineChart(document.getElementById('advanced_chart'));
-				chart.draw(data, {legend: 'none',
-								  hAxis: {maxValue: 1, minValue: 24}
-								 });
+				//Draw the chart using the data, we won't use annotations
+				chart.draw(data, {displayAnnotations: false});						
 			});
 		}
 	}
 	
+	//This function will label the different graph's X axis, as well as naming the Title of the graph
+	function label_graph(data, graph_type, rd, from_date, to_date){
+	
+		//Since there could be many data points, we may have to find a percentage distance to label from
+		//Find the length of rd to find how far labels should be placed.
+		
+		if(graph_type == "Hours"){
+			//grab the hour of the beginning date
+			//var beginning_hour = from_date.getHours();
+
+			//For each data point, add it to the graph, separated by an hour
+			//var i = 0;
+			var new_from_date = Date.strToDate(from_date)
+			for(i = 0; i < 24 ;i++){
+				
+				//data.addRow([(Date.strToDate(from_date)).changeTime('Hours', i), rd[i]]);
+				data.addRow([new Date(new_from_date.getFullYear(), new_from_date.getMonth(), new_from_date.getDate(), new_from_date.getHours(), new_from_date.getMinutes(), new_from_date.getSeconds(), new_from_date.getMilliseconds()), rd[i]]);
+				//add an hour each time through.
+				new_from_date.changeTime('Hours', 1);
+			}
+			
+		}else if(graph_type == "Month"){
+
+		}else if(graph_type == "FullYear"){
+
+		}else if(graph_type == "YTD"){
+
+		}else{
+			//Don't know how you got here
+		}	
+	}
 	
 	function reload_todays_usage() {
 		if (tab_index != 0) {
 			var i = tab_index - 1;
-			console.log("We are in reload_todays_usage")
-			$j.post('/ajax_update', {building: abr, type: 'todays_usage'/*, from: from_date, to: to_date*/}, function(response_data) {
+			
+			$j.post('/ajax_update', {building: abr, type: 'other'/*, from: from_date, to: to_date*/}, function(response_data) {
 				var d = eval(response_data);
 				var l = tag_lookup[i].toLowerCase();
 				var ll = usage_lookup[i];
 				console.log(d);
 				
 				$j('#' + l + '_current_usage').html(d.current);
-				$j('#' + l + '_usage').html(roundNumber(d.usage, 2));
-				$j('#' + l + '_use_sqft').html(roundNumber(d.usage / d.sqft, 4));
-				$j('#' + l + '_use_occupant').html(roundNumber(d.usage / d.occupants, 2));
-				$j('#' + l + '_green_house').html(roundNumber(0.00068956 * d.usage, 2)); // emission factor: 6.8956 x 10^(-4) metric tons CO2 / kWh
+				$j('#' + l + '_usage').html(roundNumber(d[ll], 2));
+				$j('#' + l + '_use_sqft').html(roundNumber(d[ll] / d.sqft, 4));
+				$j('#' + l + '_use_occupant').html(roundNumber(d[ll] / d.occupants, 2));
+				$j('#' + l + '_green_house').html(roundNumber(0.00068956 * d[ll], 2)); // emission factor: 6.8956 x 10^(-4) metric tons CO2 / kWh
 			});
 		}
 	}
@@ -97,7 +117,7 @@ window.onload = function() {
 			draw_chart();
 			clearInterval(refresher);
 			//refresher = setInterval(function() {
-			reload_todays_usage();
+				reload_todays_usage();
 			//}, 10000);
 		}
 	});
--- rails_app/public/javascripts/global.js
+++ rails_app/public/javascripts/global.js
@@ -8,6 +8,7 @@ Date.prototype.changeTime = function (incr, dif) {
 	return this;
 }
 
+//Takes a Date object, converts it to a string that is safe for insertion into a database
 Date.prototype.sqlSafeStr = function (param) {
 	var str = this.getFullYear() + '-'
 		+ (this.getMonth() + 1) + '-'
@@ -17,6 +18,46 @@ Date.prototype.sqlSafeStr = function (param) {
 		+ this.getSeconds();
 	return str;
 }
+
+//Takes a string representation of the Date, and returns a Date object
+Date.strToDate = function(date_string){
+	//date_string looks like:  "2012-5-2 18:53:56"
+
+	//Date object looks like:  "Date { Wed May 02 2012 18:53:56 GMT-0700 (PDT) }"
+	var new_date = new Date();
+	
+	//Goal is to convert the date_string into the new_date and return it.
+	
+	//Replace all the spaces using colons
+	date_string = date_string.replace(/\ /g, ":");
+	
+	//Replace all the dashes using colons
+	date_string = date_string.replace(/\-/g, ":");
+	
+	//Lets split up the string by ":"
+	var date_attributes = date_string.split(":");
+	
+	//Sets the year
+	new_date.setFullYear(parseInt(date_attributes[0]));
+	
+	//Sets the month
+	new_date.setMonth(parseInt(date_attributes[1]-1));
+	
+	//Sets the day of the month
+	new_date.setDate(parseInt(date_attributes[2]));
+	
+	//Sets the hour
+	new_date.setHours(parseInt(date_attributes[3]));
+	
+	//Sets minutes 
+	new_date.setMinutes(parseInt(date_attributes[4]));
+	
+	//Sets seconds
+	new_date.setSeconds(parseInt(date_attributes[5]));
+	
+	return new_date;	
+}
+
 
 /*
 now = new Date();
