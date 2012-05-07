4ef96952bfaeb7e4cc985cce840a3f848f0a9a5d
eelmenhurst12 <eelmenhurst12@my.whitworth.edu>
2012-05-06 18:01:56 -0700

    Fixed bugs in DataCleaner.cs... 1. Fixed decision on alarms 2. Fixed endpoints 3. Fixed reading meter instead of power

--- PowerStormReadingGatherer/PowerStormReadingGatherer/DataCleaner.cs
+++ PowerStormReadingGatherer/PowerStormReadingGatherer/DataCleaner.cs
@@ -21,16 +21,7 @@ namespace PowerStormReadingGatherer
 		
 		public void CleanData()
         {
-            // 0 -> Index
-            // 1 -> TimeOfSample
-            // 2 -> Sequence
-            // 3 -> ValueType
-            // 4 -> SampleValue
-			
-			//this is for adding the column validity in the database
-			//MySqlCommand createColumn = new MySqlCommand("ALTER TABLE electricity_readings ADD validity VARCHAR(20)", dbPowerstorm);
-			//createColumn.ExecuteNonQuery();
-
+            //BEGINS BY GRABBING ALL UNMARKED VALDITIY ROWS
             MySqlCommand command = new MySqlCommand("SELECT * FROM powerstorm_data.electricity_readings WHERE validity IS NULL OR validity = ''",dbPowerstorm); 
             MySqlDataReader read = command.ExecuteReader();
 			
@@ -47,10 +38,17 @@ namespace PowerStormReadingGatherer
             //foreach (DataRow myRow in schemaTable.Rows)
 			for (int i = 0; i < rowMeterId.Count; i++)
             {
-				
+				//THEN FOR THOSE IT FINDS ALL THE ALARMED ONES
                 int meterId = rowMeterId[i];//SEQUENCE = ID ?
                 DateTime date = rowDate[i];
-                MySqlCommand command2 = new MySqlCommand(@"SELECT id FROM electricity_readings WHERE power > ANY(SELECT (4 * STD(power) + AVG(power)) AS UPPERLIMIT FROM electricity_readings WHERE meter_id = @meter AND date_time BETWEEN @from AND  @to) AND date_time BETWEEN @from AND  @to AND (validity = '' OR validity IS NULL)", dbPowerstorm);
+                MySqlCommand command2 = new MySqlCommand(@"SELECT id FROM electricity_readings 
+WHERE power > ANY(SELECT (2 * STD(power) + AVG(power) + 2) AS UPPERLIMIT 
+FROM electricity_readings WHERE meter_id = @meter 
+AND date_time BETWEEN @from AND  @to 
+AND (validity = 'ACCEPTABLE' OR validity = 'CORRECTED')) 
+AND date_time BETWEEN @from AND  @to 
+AND (validity = '' OR validity IS NULL)
+AND  meter_id = @meter", dbPowerstorm);
                 command2.Parameters.Add(@"@meter", MySqlDbType.Int32);//CHECK TYPES
                 command2.Parameters.Add(@"@from", MySqlDbType.DateTime);
                 command2.Parameters.Add(@"@to", MySqlDbType.DateTime);
@@ -69,6 +67,7 @@ namespace PowerStormReadingGatherer
 				reader.Close();
 				MySqlCommand command3 = new MySqlCommand("",dbPowerstorm);
 				command3.Parameters.Add(@"@theId", MySqlDbType.Int32);
+                //THEN IT NOTES THEM AS ALARMED
 				foreach (int theId in theIds)
 				{
 					command3.CommandText=@"UPDATE electricity_readings SET validity = 'ALARMED' WHERE id = @theId";
@@ -78,8 +77,15 @@ namespace PowerStormReadingGatherer
 				}
 				
 							
-				
-                command2.CommandText = @"SELECT id FROM electricity_readings WHERE power > ANY(SELECT (4 * STD(power) + AVG(power)) AS UPPERLIMIT FROM electricity_readings WHERE meter_id = @meter AND date_time BETWEEN @from AND  @to) AND date_time BETWEEN @from AND  @to AND (validity = '' OR validity IS NULL)";
+				//FINDS THE OK ONES
+                command2.CommandText = @"SELECT id FROM electricity_readings 
+WHERE power <= ANY(SELECT (2 * STD(power) + AVG(power) + 2) AS UPPERLIMIT 
+FROM electricity_readings WHERE meter_id = @meter 
+AND date_time BETWEEN @from AND  @to 
+AND (validity = 'ACCEPTABLE' OR validity = 'CORRECTED')) 
+AND date_time BETWEEN @from AND  @to 
+AND (validity = '' OR validity IS NULL)
+AND  meter_id = @meter";
                 MySqlDataReader reader2 = command2.ExecuteReader();
 				theIds = new List<int>();
 				while (reader2.Read())
@@ -91,6 +97,7 @@ namespace PowerStormReadingGatherer
 				command4.Parameters.Add(@"@theId", MySqlDbType.Int32);
 				foreach (int theId in theIds)
 				{
+                    //MARKS THEM AS OK
 					command4.CommandText=@"UPDATE electricity_readings SET validity = 'ACCEPTABLE' WHERE id = @theId";
 					command4.Parameters["@theId"].Value = theId;
 
@@ -99,7 +106,8 @@ namespace PowerStormReadingGatherer
 
             }
 			 
-			 
+			//BEGINS CLEANING THE DATA BY GRABBING ALL ALARMED ONES 
+            
             MySqlCommand command5 = new MySqlCommand("SELECT * FROM electricity_readings WHERE validity = 'ALARMED'", dbPowerstorm); 
             MySqlDataReader alarmedReader = command5.ExecuteReader();
 
@@ -130,22 +138,28 @@ namespace PowerStormReadingGatherer
 				command5.Parameters["@date"].Value = dates[i].ToString();
 				
                 alarmedReader = command5.ExecuteReader();
-				
-				alarmedReader.Read();
-              //  DataTable data = reader.GetSchemaTable();
 
-               // DataRow trow = data.Rows[0];
-			//	Object trow = theRow[0];                         
-             //   long datePrev = long.Parse(trow[1].ToString());
-               // int powerPrev = Int32.Parse(trow[4].ToString());
-			
-				//double datePrev = Convert.ToDouble(alarmedReader[1].ToString());
-			//	string datePrv = alarmedReader[1].ToString();
-			//	DateTime dtPrev = Convert.ToDateTime(datePrv);
-			//	double datePrev = Convert.ToDouble(dtPrev);
-				double datePrev = DateTime.Parse(alarmedReader[1].ToString()).Ticks;
-            //    long datePrev = long.Parse(alarmedReader[1].ToString());
-				int powerPrev = Int32.Parse(alarmedReader[2].ToString());
+                bool hadPrev = false;
+                double datePrev = 0;
+                int powerPrev = 0;
+                if (alarmedReader.Read())
+                {
+                    //  DataTable data = reader.GetSchemaTable();
+
+                    // DataRow trow = data.Rows[0];
+                    //	Object trow = theRow[0];                         
+                    //   long datePrev = long.Parse(trow[1].ToString());
+                    // int powerPrev = Int32.Parse(trow[4].ToString());
+
+                    //double datePrev = Convert.ToDouble(alarmedReader[1].ToString());
+                    //	string datePrv = alarmedReader[1].ToString();
+                    //	DateTime dtPrev = Convert.ToDateTime(datePrv);
+                    //	double datePrev = Convert.ToDouble(dtPrev);
+                    datePrev = DateTime.Parse(alarmedReader[1].ToString()).Ticks;
+                    //    long datePrev = long.Parse(alarmedReader[1].ToString());
+                    powerPrev = Int32.Parse(alarmedReader[3].ToString());
+                    hadPrev = true;
+                }
 				alarmedReader.Close();
 				
                 //Recent
@@ -158,37 +172,44 @@ namespace PowerStormReadingGatherer
                 alarmedReader = command5.ExecuteReader();
              //   DataTable data = reader.GetSchemaTable();
                // DataRow trow = data.Rows[0];
-				
-				alarmedReader.Read();
-				
-			//	double dateLast = Convert.ToDouble(alarmedReader[1].ToString());
-				double dateLast = DateTime.Parse(alarmedReader[1].ToString()).Ticks;
-			//	long dateLast = long.Parse(alarmedReader[1].ToString());
-				int powerLast = Int32.Parse(alarmedReader[2].ToString());
-				
+
+                double dateLast = 0;
+                int powerLast = 0;
+                bool hadLast = false;
+                if (alarmedReader.Read())
+                {
+
+                    //	double dateLast = Convert.ToDouble(alarmedReader[1].ToString());
+                    dateLast = DateTime.Parse(alarmedReader[1].ToString()).Ticks;
+                    //	long dateLast = long.Parse(alarmedReader[1].ToString());
+                    powerLast = Int32.Parse(alarmedReader[3].ToString());
+                    hadLast = true;
+                }
+                    alarmedReader.Close();
                // long dateLast = long.Parse(trow[1].ToString());
                 //int powerLast = Int32.Parse(trow[4].ToString()); ;
 
+                    if (hadLast && hadPrev)
+                    {
+                        double currentTime = DateTime.Parse(dates[i].ToString()).Ticks;
+                        int id = Int32.Parse(idList[i].ToString());
 
-                double currentTime = DateTime.Parse(dates[i].ToString()).Ticks;
-                int id = Int32.Parse(idList[i].ToString());
+                        double newPower = powerPrev + (powerLast - powerPrev) * ((currentTime - datePrev) / (dateLast - datePrev));
+                        //CHECK IT IS CALLED ID next INSERT
+                        //update with newPower
+                        MySqlCommand updateCommand = new MySqlCommand(@"UPDATE electricity_readings SET power = @power, validity = 'CORRECTED' WHERE id = @id", dbPowerstorm);
+                        updateCommand.Parameters.Add(@"@power", MySqlDbType.Int32);
+                        updateCommand.Parameters.Add(@"@id", MySqlDbType.Int32);
 
-                double newPower = powerPrev + (powerLast - powerPrev) * ((currentTime - datePrev) / (dateLast = datePrev));
-                //CHECK IT IS CALLED ID next INSERT
-                //update with newPower
-               MySqlCommand updateCommand = new MySqlCommand(@"UPDATE electricity_readings SET power = @power WHERE id = @id", dbPowerstorm);
-                updateCommand.Parameters.Add(@"@power", MySqlDbType.Int32);
-                updateCommand.Parameters.Add(@"@id", MySqlDbType.Int32);
-                
-                updateCommand.Parameters["@power"].Value = newPower;
-                updateCommand.Parameters["@id"].Value = id;
+                        updateCommand.Parameters["@power"].Value = newPower;
+                        updateCommand.Parameters["@id"].Value = id;
 
-                updateCommand.ExecuteNonQuery();    
-			 
+                        updateCommand.ExecuteNonQuery();
+                    }
 			 
 			// updateCommand = new MySqlCommand(@"UPDATE electricity_readings SET power = @power, validity = 'CORRECTED' WHERE id = @id", dbPowerstorm);
-			 	updateCommand = new MySqlCommand(@"UPDATE electricity_readings SET power = @power, validity = 'CORRECTED' WHERE id = @id", dbPowerstorm);
-				updateCommand.ExecuteNonQuery();
+			 	//updateCommand = new MySqlCommand(@"UPDATE electricity_readings SET power = @power, validity = 'CORRECTED' WHERE id = @id", dbPowerstorm);
+				//updateCommand.ExecuteNonQuery();
 
             }
            
